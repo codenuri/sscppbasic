29da925e99ed31abc507333b43108b0e1b43d7fe
ecourse <smkang@codenuri.co.kr>
2022-12-05 11:04:29 +0900

    Add files via upload

---
+++ DAY1/3_변수3.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+struct Point
+{
+	int x, y;
+};
+void foo(int n)    {} // "int n = {3}"
+void goo(Point pt) {}
+
+int main()
+{
+	Point p = { 1,2 };
+
+	foo(3);
+	foo( { 3 } ); // C++11 부터 가능
+
+	goo(p);	
+	goo( {1, 2} ); // C++11 부터 가능. Point pt = {1,2}
+}
+
+
+
+
+
