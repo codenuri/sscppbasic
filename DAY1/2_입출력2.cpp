3e1747ae61df88f7df7184c86472ca1d06d32ef4
ecourse <smkang@codenuri.co.kr>
2022-12-05 10:40:08 +0900

    Add files via upload

---
+++ DAY1/2_입출력2.cpp
@@ -0,0 +1,17 @@
+﻿// 3_입출력2.cpp - 15 page아래 부분
+#include <iostream>
+#include <iomanip> // 입출력 조정자 함수를 사용하기 위해!
+					// std::setw(), std::setfill()등.. 
+int main()
+{
+	int n = 10;
+	std::cout << n << std::endl; // 10진수
+
+	std::cout << std::hex << n << std::endl; // 16진수 출력
+
+	std::cout << std::dec << n << std::endl; // 
+
+	std::cout << std::setw(10) << n << std::endl; // 
+
+	std::cout << std::setw(10) << std::setfill('#') << n << std::endl; // 
+}
