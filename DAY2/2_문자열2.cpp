6f9e6014862bb459de28fb0d2a9f281865afd426
ecourse <smkang@codenuri.co.kr>
2022-12-06 08:43:58 +0900

    Add files via upload

---
+++ DAY2/2_문자열2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+
+int main()
+{
+	// C 언어의 문자열 : char 배열, char 포인터.
+	// char s1[] = "ABCD";
+	// char s2[5];
+
+	// C++ 언어의 문자열 : std::string 타입사용
+
+	std::string s1 = "ABCD";
+	std::string s2;
+
+	s2 = s1;	
+
+	if (s2 == s1)	
+	{				
+					
+					
+	}
+}
