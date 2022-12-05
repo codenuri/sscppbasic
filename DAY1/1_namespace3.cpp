018712db6e124e1252697b3d709307facb08ae8e
ecourse <smkang@codenuri.co.kr>
2022-12-05 09:56:34 +0900

    Add files via upload

---
+++ DAY1/1_namespace3.cpp
@@ -0,0 +1,17 @@
+﻿// 1_namespace3.cpp - 9page 내용
+// 
+#include <algorithm>
+using namespace std;
+
+// 핵심 : 모든 C++ 표준(함수, 클래스)은 "std::" 라는 이름 공간안에
+//       있습니다.
+
+int count = 10;
+
+int main()
+{
+	int ret = count; // 왜?? 에러일까요 ?
+
+	int n1 = min(3, 2); // C++ 표준 함수. algorithm 헤더 필요
+	int n2 = std::min(3, 2); // ok. 
+}
