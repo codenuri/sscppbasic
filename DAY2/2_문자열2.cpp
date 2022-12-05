20cd24dd6cbd04d78967955b9ff4cd0dc702deb3
ecourse <smkang@codenuri.co.kr>
2022-12-06 08:46:26 +0900

    Add files via upload

--- DAY2/2_문자열2.cpp
+++ DAY2/2_문자열2.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <cstring>
-#include <string>
+#include <cstring> // C 언어의 string.h
+#include <string>  // C++의 std::string 사용위해
 
 int main()
 {
@@ -9,15 +9,15 @@ int main()
 	// char s2[5];
 
 	// C++ 언어의 문자열 : std::string 타입사용
-
+	// => 클래스 라는 문법으로 만들어진 타입
+	// => 정수형 변수를 사용하는 것처럼 직관적으로 사용가능.
 	std::string s1 = "ABCD";
 	std::string s2;
-
+		
 	s2 = s1;	
-
 	if (s2 == s1)	
-	{				
-					
-					
+	{										
 	}
+	s2 = s1 + s2; // "s1 * 3" 는 안됩니다.
+	std::cout << s2 << std::endl;
 }
