454f8ef4b2a602c1e5f6d3f25381759ebbf6b7ac
ecourse <smkang@codenuri.co.kr>
2022-12-05 11:14:15 +0900

    Add files via upload

---
+++ DAY1/3_변수5.cpp
@@ -0,0 +1,16 @@
+﻿// 3_변수5
+// 23 page ~
+
+// typedef int DWORD;
+// typedef void(*PF)(); // 함수 포인터 타입
+
+// C++11 부터는 typedef 대신 using 사용하세요
+using DWORD = int;
+using PF = void(*)();
+
+int main()
+{
+	DWORD n; // int n
+	PF    f; //
+}
+
