765d3a87f01c4c4b7076fa29fdd95e7765b8b7b5
ecourse <smkang@codenuri.co.kr>
2022-12-05 14:02:08 +0900

    Add files via upload

---
+++ DAY1/4_함수6.cpp
@@ -0,0 +1,21 @@
+﻿// 4_함수6. 후위 반환 타입
+
+// C/C++ 언어에서는 전통적으로 함수 반환 타입을 먼저 표기 합니다.
+/*
+int square(int a)
+{
+	return a * a;
+}
+*/
+// 아래 코드가 C++11 부터 나온 "후위 반환 타입(suffix return type)"
+// 이라는 문법 입니다.
+// swift, rust, python, haskell 등의 언어가 이 방식 입니다.
+auto square(int a) -> int
+{
+	return a * a;
+}
+
+int main()
+{
+	square(3);
+}
