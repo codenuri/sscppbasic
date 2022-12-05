45d672ff1e9c13fa4d575ce51a49df6b11440fff
ecourse <smkang@codenuri.co.kr>
2022-12-05 13:56:20 +0900

    Add files via upload

--- DAY1/4_함수5.cpp
+++ DAY1/4_함수5.cpp
@@ -14,11 +14,22 @@ int gcd(int a, int b)
 								// => 의도적으로 gcd(실수)를 사용할수
 								//    없게하는 것!
 
-double gcd(double a, double b) = delete; // C++11 의 함수 삭제라는 문법
+//double gcd(double a, double b) = delete; // C++11 의 함수 삭제라는 문법
 							// 삭제된 함수를 사용하면 "컴파일에러 발생"
+
+// 아래 처럼 하면
+// 1. gcd(int, int) 는 위에 있으므로 사용시 에러 아닙니다.
+// 2. 나머지 모든 버전은 삭제됩니다.
+template<typename T> T gcd(T, T) = delete;
+
+// 그런데, 함수 삭제 문법은 "일반 함수" 보다는
+// 컴파일러가 자동으로 만드는 클래스의 멤버 함수를 삭제할때 주로 사용합니다.
+
 int main()
 {
 	gcd(1, 2);
 
 	gcd(2.3, 3.2);  // error를 나오게 하고 싶습니다..
 }
+
+
