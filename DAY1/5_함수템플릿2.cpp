2b73f9f8bc72e3bb62b66127b3a4d6e419b0c8b9
ecourse <smkang@codenuri.co.kr>
2022-12-05 14:12:37 +0900

    Add files via upload

--- DAY1/5_함수템플릿2.cpp
+++ DAY1/5_함수템플릿2.cpp
@@ -11,6 +11,10 @@ int main()
 	// 아래 표기법이 정확한 표기법이지만
 	std::cout << Add<int, double>(3,  2.1) << std::endl;
 
+	// T1=int 즉, 전달한 타입사용
+	// T2는 2.1을 보고 추론! (double)
+	std::cout << Add<int>(3, 2.1) << std::endl;
+
 	// 타입 인자는 생략가능합니다.
 	std::cout << Add(3, 2.1) << std::endl;
 }
