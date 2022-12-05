8dfef427b3508bd8767415d92ef570cf0527570d
ecourse <smkang@codenuri.co.kr>
2022-12-05 14:08:51 +0900

    Add files via upload

--- DAY1/5_함수템플릿1.cpp
+++ DAY1/5_함수템플릿1.cpp
@@ -8,7 +8,13 @@ T Add(T a, T b)
 
 int main()
 {
-	std::cout << Add(1,   2 )    << std::endl;
+	std::cout << Add(1,   2 )  << std::endl;
 	std::cout << Add(3.1, 2.1) << std::endl;
+
+	std::cout << Add(3, 2.1) << std::endl; // error
+								// 함수 인자로 T의 타입을 결정할수 없다
+								// error
+
+	std::cout << Add<double>(3, 2.1) << std::endl; // ok..
 }
 
