586b40c323588bea9892595aa2423e1f100f4ed6
ecourse <smkang@codenuri.co.kr>
2022-12-06 16:39:48 +0900

    Add files via upload

--- DAY2/10_정적멤버7.cpp
+++ DAY2/10_정적멤버7.cpp
@@ -6,14 +6,24 @@
 
 class Date
 {
-public:
+private:
 	int year;
 	int month;
 	int day;
-};
+public:
+	Date(int y, int m, int d) : year(y), month(m), day(d) {}
 
+	void set_month(int m)
+	{
+		if (m <= 12 && m >= 1)
+			month = m;
+	}
+	int get_month() { return month; }
+
+	// year, day 에 대해서도 getter/setter 제공하세요..
+};
 
 int main()
 {
-
+	Date d1(2022, 12, 6);
 }
