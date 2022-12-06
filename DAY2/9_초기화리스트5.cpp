0f72b15c6230847ee5b420f6e60d94537e5a2d49
ecourse <smkang@codenuri.co.kr>
2022-12-06 14:53:40 +0900

    Add files via upload

---
+++ DAY2/9_초기화리스트5.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+// member field initialization
+class Point
+{
+	// C++11 부터는 멤버 데이타를 직접 초기화 할수 있습니다.
+	int x = 0;
+	int y = 0;
+public:
+	Point() {}
+	Point(int a) : y(a) {}
+};
+
+// 위 클래스를 컴파일하면 아래 처럼 변경됩니다.
+class Point
+{
+	int x;
+	int y;
+public:
+	// 필드 초기화 값을 가지고 아래 처럼 생성자를 변경하는 것. 
+	Point()      : x(0), y(0) {}
+	Point(int a) : x(0), y(a) {}
+};
+
+int main()
+{
+
+}
+
+
+
+
