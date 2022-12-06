6a4b2038687c534441fdd1a3dc1d2c917b8eca69
ecourse <smkang@codenuri.co.kr>
2022-12-06 15:41:40 +0900

    Add files via upload

---
+++ DAY2/10_정적멤버2.cpp
@@ -0,0 +1,23 @@
+﻿// 3_정적멤버1 - 110 page
+#include <iostream>
+
+// 방법 2. 전역변수 cnt 사용
+int cnt = 0;
+
+class Car
+{
+	int color;
+	int speed;
+public:
+	Car() { ++cnt; }
+	~Car() { --cnt; }
+};
+int main()
+{
+	Car c1;
+	Car c2;
+
+	std::cout << cnt << std::endl;  // 2
+}
+
+
