1634688f945cc01647c5177867bafb5b07c14010
ecourse <smkang@codenuri.co.kr>
2022-12-06 09:52:18 +0900

    Add files via upload

---
+++ DAY2/5_OOP5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+struct Rect
+{
+	int left;
+	int top;
+	int right;
+	int bottom;
+
+	int getArea() { return (right - left) * (bottom - top); }
+	void draw() { std::cout << "draw rect" << std::endl; }
+};
+
+int main()
+{
+
+}
