2a15ef9702341b59957b5c2dfcc0789cf0a779f7
ecourse <smkang@codenuri.co.kr>
2022-12-06 09:14:39 +0900

    Add files via upload

---
+++ DAY2/5_OOP1.cpp
@@ -0,0 +1,18 @@
+// 3_OOP1 - 57page
+#include <iostream>
+
+int getRectArea(int left, int top, int right, int bottom)
+{
+	return (right - left) * (bottom - top);
+}
+
+void drawRect(int left, int top, int right, int bottom)
+{
+	std::cout << "draw rect" << std::endl;
+}
+
+int main()
+{
+	int n1 = getRectArea(1, 1, 10, 10);
+	drawRect(1, 1, 10, 10);
+}
