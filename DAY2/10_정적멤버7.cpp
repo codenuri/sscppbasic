390ef3693a4a726aa54fb6b5dcc13eb0ebac73a9
ecourse <smkang@codenuri.co.kr>
2022-12-06 16:34:20 +0900

    Add files via upload

---
+++ DAY2/10_정적멤버7.cpp
@@ -0,0 +1,19 @@
+// 10_정적멤버7
+#include <iostream>
+
+// 프로그램에서 날짜를 다루는 경우가 많다.
+// => "날짜(Date)" 타입을 먼저 만들자..
+
+class Date
+{
+public:
+	int year;
+	int month;
+	int day;
+};
+
+
+int main()
+{
+
+}
