4ba75f909765e44d04f366826c3e451b7b375c81
ecourse <smkang@codenuri.co.kr>
2022-12-06 09:57:18 +0900

    Add files via upload

--- DAY2/5_OOP5.cpp
+++ DAY2/5_OOP5.cpp
@@ -7,11 +7,18 @@ struct Rect
 	int right;
 	int bottom;
 
-	int getArea() { return (right - left) * (bottom - top); }
-	void draw() { std::cout << "draw rect" << std::endl; }
+	int getArea() // 컴파일하면 int getArea(Rect* this) 처럼 인자가 생기게됩닏.
+	{ 
+		return (right - left) * (bottom - top);  // (this->right - this->left)
+	}	
 };
 
 int main()
 {
+	Rect r1 = { 1,1,10,10 };
+	Rect r2 = { 5,5,10,10 };
 
+	r1.getArea(); // 이순간의 원리를 생각해 봅시다.
+				  // getArea(&r1);
+	r2.getArea(); // getArea(&r2)
 }
