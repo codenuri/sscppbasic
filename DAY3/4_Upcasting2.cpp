37a4b2e71c785c65831a0bc187a12c4b394c14bf
ecourse <smkang@codenuri.co.kr>
2022-12-07 11:43:09 +0900

    Add files via upload

--- DAY3/4_Upcasting2.cpp
+++ DAY3/4_Upcasting2.cpp
@@ -22,3 +22,10 @@ int main()
 	std::vector<Animal*> v2; // Animal 로 부터 파생된 모든 동물을 보관가능
 							// 동종을 보관하는 컨테이너
 }
+
+// Upcasting 활용 2
+//void NewYear(Dog* p)	// Dog 만 전달 가능한 함수
+void NewYear(Animal* p) // 모든 동물을 전달 가능한 함수.
+{
+	++(p->age);
+}
