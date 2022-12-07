9895ac5d439d71eb7e5ae44416ee4f42068a316c
ecourse <smkang@codenuri.co.kr>
2022-12-07 16:59:45 +0900

    Add files via upload

--- DAY3/9_인터페이스2.cpp
+++ DAY3/9_인터페이스2.cpp
@@ -4,41 +4,34 @@
 // 카메라와 카메라 사용자가 지켜야 하는 규칙(인터페이스)를 먼저 설계 합니다.
 
 // 규칙 : 모든 카메라는 아래 클래스로 부터 파생 되어야 한다.
+//       => 모든 카메라는 반드시 "take()" 함수를 만들라는 의미!
 class ICamera
 {
 public:
 	virtual void take() = 0;
 };
+// 진짜 카메라는 아직 없지만, 명확한 규칙(인터페이스)가 있습니다.
+// 사용자는 규칙 대로만 사용하면 됩니다.
 
+class Person
+{
+public:
+	void use_camera(ICamera* p) { p->take(); }
+};
 
-
-
-
-
-
-
-
-
-
-
-class Camera
+// 이제 모든 카메라는 규칙을 지켜야 합니다
+class Camera : public ICamera
 {
 public:
 	void take() { std::cout << "take picture" << std::endl; }
 };
 
-class HDCamera
+class HDCamera : public ICamera
 {
 public:
 	void take() { std::cout << "take HD picture" << std::endl; }
 };
 
-class Person
-{
-public:
-	void use_camera(Camera* p) { p->take(); }
-	void use_camera(HDCamera* p) { p->take(); }
-};
 int main()
 {
 	Person p;
