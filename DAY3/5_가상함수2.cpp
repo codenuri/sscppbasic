b4cdc2d126c87965b7224043dfbd5f01b1ad438c
ecourse <smkang@codenuri.co.kr>
2022-12-07 12:03:58 +0900

    Add files via upload

---
+++ DAY3/5_가상함수2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+class Animal
+{
+public:
+	void Cry1() { std::cout << "Animal Cry1" << std::endl; }
+	void Cry2() { std::cout << "Animal Cry2" << std::endl; }
+};
+
+class Dog : public Animal
+{
+public:
+	void Cry1() { std::cout << "Dog Cry1" << std::endl; }
+	void Cry1() { std::cout << "Dog Cry2" << std::endl; }
+};
+int main()
+{
+	Animal a; 
+	Dog    d; 
+
+	Animal* p = &d;
+
+	p->Cry1();
+	p->Cry2();
+}
