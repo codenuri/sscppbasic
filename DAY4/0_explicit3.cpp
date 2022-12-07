45e480ff30d03c58480860826e1ccd1e1ffee64c
ecourse <smkang@codenuri.co.kr>
2022-12-08 08:55:20 +0900

    Add files via upload

---
+++ DAY4/0_explicit3.cpp
@@ -0,0 +1,21 @@
+class Point
+{
+	int x;
+	int y;
+public:
+	// C++11 이후에는 인자가 1개뿐 아니라 
+	// 모든 생성자 앞에 explicit 를 붙일수 있게 되었습니다.
+	Point() {}
+	Point(int x) : x(x), y(0) {}
+	Point(int x, int y) : x(x), y(y) {}
+};
+void foo(Point pt) {}
+
+int main()
+{
+	// C++11 부터는 {} 초기화 때문에 아래 코드가 모두 가능합니다.
+	foo({});
+	foo(1);
+	foo({1});
+	foo({1,2});
+}
