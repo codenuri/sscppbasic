fb551542fc683f922cd89eeb26da242f6d985c43
ecourse <smkang@codenuri.co.kr>
2022-12-07 13:51:41 +0900

    Add files via upload

--- DAY3/6_도형편집기예제2.cpp
+++ DAY3/6_도형편집기예제2.cpp
@@ -30,23 +30,22 @@ int main()
 		int cmd;
 		std::cin >> cmd;
 
-		if (cmd == 1)
-		{
-			v.push_back(new Rect);
-		}
-		else if (cmd == 2)
-		{
-			v.push_back(new Circle);
-		}
+		if      (cmd == 1) 	v.push_back(new Rect);
+		else if (cmd == 2)	v.push_back(new Circle);
 		else if (cmd == 9)
 		{
 			// 컨테이너에 있는 모든 도형을 그린다.
-			for (auto p : v)
-				p->draw();
+			for (auto p : v)  // 여기서 p 는 "Shape*" 입니다.
+				p->draw();	  // error.  Shape 에는 draw 함수가 없습니다.
 		}
 	}
 }
 // 위 코드는 왜?? 에러일까요?
+// => 실제 객체는 Rect, Circle 이지만
+// => p의 타입은 Shape* 인데 Shape 에는 draw() 가 없다.
+// 
 // 해결책은 뭘까요 ?
+// 1. p 를 Rect* 나 Circle* 로 캐스팅해서 사용하자
+// 2. Shape 에도  draw() 만들자 => 정답.. 
 
 
