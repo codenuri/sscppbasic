3646643e1d8de1f93eacdba28c17c80b36d4feaf
ecourse <smkang@codenuri.co.kr>
2022-12-07 12:06:31 +0900

    Add files via upload

--- DAY3/5_가상함수2.cpp
+++ DAY3/5_가상함수2.cpp
@@ -3,16 +3,21 @@
 class Animal
 {
 public:
+	// 일반 멤버 함수 : static binding
 	void Cry1() { std::cout << "Animal Cry1" << std::endl; }
-	void Cry2() { std::cout << "Animal Cry2" << std::endl; }
+
+
+	// 가상(virtual)함수 : dynamic binding 을 해달라는 지시어.
+	virtual void Cry2() { std::cout << "Animal Cry2" << std::endl; }
 };
 
 class Dog : public Animal
 {
 public:
 	void Cry1() { std::cout << "Dog Cry1" << std::endl; }
-	void Cry1() { std::cout << "Dog Cry2" << std::endl; }
+	virtual void Cry2() { std::cout << "Dog Cry2" << std::endl; }
 };
+
 int main()
 {
 	Animal a; 
@@ -20,6 +25,6 @@ int main()
 
 	Animal* p = &d;
 
-	p->Cry1();
-	p->Cry2();
+	p->Cry1();	// Animal Cry1
+	p->Cry2();	// Dog    Cry2
 }
