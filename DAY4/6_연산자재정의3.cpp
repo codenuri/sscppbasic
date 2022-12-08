d699f6f411ae6aafde715f5f939e0f4cfdaba180
ecourse <smkang@codenuri.co.kr>
2022-12-08 15:32:53 +0900

    Add files via upload

--- DAY4/6_연산자재정의3.cpp
+++ DAY4/6_연산자재정의3.cpp
@@ -18,6 +18,7 @@ public:
 	*/
 	friend Point operator+(const Point& p1, const Point& p2);
 };
+
 Point operator+(const Point& p1, const Point& p2)
 {
 	std::cout << "non-member operator+" << std::endl;
@@ -25,6 +26,7 @@ Point operator+(const Point& p1, const Point& p2)
 	Point temp(p1.x + p2.x, p1.y + p2.y);
 	return temp;
 }
+
 int main()
 {
 	const Point p1(1, 1);
@@ -33,5 +35,15 @@ int main()
 	Point p3 = p1 + p2; // operator+(p1, p2) 가 있어도 됩니다.
 	
 	operator+(p1, p2);
+
+	// member vs 일반함수
+	// member 의 장점 : private 멤버 접근이 쉽다
+	// 일반 함수      : 1번째 인자가 객체 아닐때도 동작한다.
+
+	int n = 0;
+
+	Point p5 = p1 + p2;
+	Point p6 = p1 + n;
+	Point p7 = n  + p1;
 }
 
