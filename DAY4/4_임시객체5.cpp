3344485f1ed43e333666636487189a15c9099cda
ecourse <smkang@codenuri.co.kr>
2022-12-08 14:35:36 +0900

    Add files via upload

--- DAY4/4_임시객체5.cpp
+++ DAY4/4_임시객체5.cpp
@@ -5,17 +5,20 @@ class Counter
 {
 	int cnt = 0;
 public:
-	void Increment()
+	Counter Increment()
 	{
 		++cnt;
+		return *this;
 	}
 	int get() const { return cnt; }
 };
 int main()
 {
 	Counter c;
-	c.Increment();
-	c.Increment();
-	c.Increment();
+//	c.Increment();
+//	c.Increment();
+//	c.Increment();
+	c.Increment().Increment().Increment();
+
 	std::cout << c.get() << std::endl;
 }
