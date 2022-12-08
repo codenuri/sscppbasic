92c12299be07f052ab2246ae5fd851fb8d0a348e
ecourse <smkang@codenuri.co.kr>
2022-12-08 15:36:54 +0900

    Add files via upload

--- DAY4/6_연산자재정의3.cpp
+++ DAY4/6_연산자재정의3.cpp
@@ -42,8 +42,16 @@ int main()
 
 	int n = 0;
 
-	Point p5 = p1 + p2;
-	Point p6 = p1 + n;
-	Point p7 = n  + p1;
+	Point p5 = p1 + p2; // p1.operator+(Point)
+	Point p6 = p1 + n;	// p1.operator+(int)
+	Point p7 = n  + p1; // n.operator+(Point) 가 필요한데..
+						// n은 int 이므로 멤버 함수를 만들수 없다.
+						// 이 경우 operator+(int, Point) 를 만들며 된다.
+
+	// 멤버 함수로의 구현은 
+	// => 1번째 인자가 user define type이 아닌것은 할수 없다.
+
+	// 일반 함수는
+	// => 1번째 인자가 user define type이 아니어도 할수 있다.
 }
 
