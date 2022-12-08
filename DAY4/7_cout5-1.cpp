3b8a991bca71bef27ecbc430836606bba8fc028b
ecourse <smkang@codenuri.co.kr>
2022-12-08 16:46:27 +0900

    Add files via upload

--- DAY4/7_cout5-1.cpp
+++ DAY4/7_cout5-1.cpp
@@ -12,8 +12,16 @@ public:
 	friend std::ostream& operator<<(std::ostream& os, const Point& p);
 };
 
+// QA1. Point 앞에는 왜?? const 를 붙이나요 ?
+// 1. 인자를 받은 p 를 수정하지 않겠다는 의도
+// 2. 임시객체도 받겠다는 의도!
+
+// QA2. 그럼!! std::ostream& 앞에는 왜 const 가 없나요 ??
+// => main 함수 아래 코드 참고.. 
+
 std::ostream& operator<<(std::ostream& os, const Point& p)
 {
+	// 현재 os 는 cout 의 별명입니다.
 	os << p.x << ", " << p.y;
 
 	return os;
@@ -24,6 +32,9 @@ int main()
 	Point p(1, 2);
 
 	std::cout << p ; // operator<<(cout, p);
+
+	std::cout << Point(1, 2); // 되는게 좋으세요 ?? 안되는게 좋으세요
+								// operator<<(cout, Point(1,2));
 }
 
 
