c0ed6bd1e6adc7081cbbe4dab1ca0b858c0f3015
ecourse <smkang@codenuri.co.kr>
2022-12-09 11:29:36 +0900

    Add files via upload

---
+++ DAY5/3_반복자5.cpp
@@ -0,0 +1,27 @@
+// 5_¹İº¹ÀÚ1
+#include <iostream>
+#include <vector>
+#include <list>
+
+// reverse_iterator
+
+int main()
+{
+//	std::list<int>   c = { 1,2,3,4,5,6,7,8,9,10 };
+	std::vector<int> c = { 1,2,3,4,5,6,7,8,9,10 };
+
+//	auto p1 = c.begin(); 
+//	auto p2 = c.end();
+
+	auto p1 = c.rbegin();	// reverse begin
+	auto p2 = c.rend();
+
+	while (p1 != p2)
+	{
+		std::cout << *p1 << ", ";
+		++p1;
+	}
+	std::cout << std::endl;
+
+}
+
