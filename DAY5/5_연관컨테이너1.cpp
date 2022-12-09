b01a004ba8a13cd55ba390381a90fe7791191fbd
ecourse <smkang@codenuri.co.kr>
2022-12-09 14:36:30 +0900

    Add files via upload

---
+++ DAY5/5_연관컨테이너1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+
+// 선형 컨테이너3개
+#include <vector>
+#include <list>
+#include <deque>
+
+// 연관 컨테이너 4개
+#include <set>	
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+
+int main()
+{
+	std::vector<int> v = { 1,2,3,4,5,6,7,8,9,10 };
+	
+	std::set<int> s = { 50, 30, 70, 60, 20, 40, 80 };
+}
