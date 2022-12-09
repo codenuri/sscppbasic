bc15233e06e6cb1c5a904689fc4c9b88a8f6669e
ecourse <smkang@codenuri.co.kr>
2022-12-09 13:32:32 +0900

    Add files via upload

---
+++ DAY5/4_알고리즘3-1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+
+
+bool foo(int n) { return n % 3 == 0; }
+
+int main()
+{
+	std::vector<int> v = { 1,2,9,4,3,6,7,3,9,10 };
+
+	auto ret1 = std::find_if(v.begin(), v.end(), foo);
+
+}
+
